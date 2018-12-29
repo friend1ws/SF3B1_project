309d82a20f39c7578645f1f9b69e575c0a4e24c7
friend1ws <friend1ws@gmail.com>
2018-12-29 05:43:18 +0000

    modify zibb_functions.R

--- script/subscript_zibb/zibb_functions.R
+++ script/subscript_zibb/zibb_functions.R
@@ -55,6 +55,24 @@ zip_mlogLikelihood <- function(params, values) {
   
 }
 
+zib_mlogLikelihood <- function(params, values) {
+  
+  p_prob <- params[1]
+  p_pi <- params[2]
+  
+  v_n <- values[[1]]
+  v_k <- values[[2]]
+
+  zero_ind <- which(v_k == 0) 
+  non_zero_ind <- setdiff(1:length(v_k), zero_ind)
+  
+  ML <- 0
+  ML <- ML + length(non_zero_ind) * log(1 - p_pi) + sum(v_k[non_zero_ind]) * log(p_prob) + sum(v_n[non_zero_ind] - v_k[non_zero_ind]) * log(1 - p_prob)
+  ML <- ML + sum(log(p_pi + (1 - p_pi) * (1 - p_prob)^v_n[zero_ind]))
+  
+  return(-ML)
+
+}
 
 bb_loglikelihood <- function(p_alpha, p_beta, v_n, v_k) {
   
@@ -83,6 +101,14 @@ bb_optim <- function(v_n, v_k) {
   return(cret)
 }
 
+zib_optim <- function(v_n, v_k) {
+  
+  cret <- constrOptim(c(0.5, 0.2), zib_mlogLikelihood, grad=NULL, 
+                      ui = rbind(diag(2), -diag(2)), ci=c(0, 0.01, -1, -0.99), 
+                      values = list(v_n, v_k), outer.iterations = 1000, outer.eps = 1e-8)
+  return(cret)
+  
+}
 
 get_prob_for_zibb <- function(params, s_n, s_k) {
   
@@ -107,3 +133,15 @@ get_prob_for_bb <- function(params, s_n, s_k) {
 
 }
 
+get_prob_for_zib <- function(params, s_n, s_k) {
+  
+  p_prob <- params[1]
+  p_pi <- params[2]
+  
+  if (s_k == 0) {
+    return(p_pi + (1 - p_pi) * dbinom(0, s_n, p_prob))
+  } else {
+    return((1 - p_pi) * dbinom(s_k, s_n, p_prob))
+  }
+
+}
