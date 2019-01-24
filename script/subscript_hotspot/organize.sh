2121c8fabb0ca0c5869030ab47b2790dddeea60d
friend1ws <friend1ws@gmail.com>
2019-01-24 01:41:53 +0000

    slightly modified organize.sh files

--- script/subscript_hotspot/organize.sh
+++ script/subscript_hotspot/organize.sh
@@ -7,4 +7,6 @@ fi
 
 tail +2 ../output/recount2/prediction/recount2.score.merged.txt | grep -v SRP012682 | awk '$3 > 5.0 {print}' - | sort -k3 -n -r > ../output/sra/hotspot/run_id_list2.txt
 
+python subscript_hotspot/organize_result.py output_s3/SF3B1.hotspot.result.txt ../db/SF3B1_hotspot_COSMIC/COSMIC_SF3B1_hotspot.txt ../output/sra/hotspot/run_id_list2.txt > ../output/sra/hotspot/SF3B1.hotspot.result.info.txt
+
 
